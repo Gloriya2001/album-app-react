a2a5c768a12b12a24cdf316247d0c9f7ac8bf017
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-08 01:30:09 +0530

    Delete done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
 import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <div>
-    
-<Add/>
-<br></br>
-<Search/>
-<br></br>
+
+      <Add />
+      <br></br>
+      <Search />
+      <br></br>
+      <Delete />
+      <br></br>
     </div>
   );
 }
