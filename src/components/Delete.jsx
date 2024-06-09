9453e50c540798337b085acbf7abfa6e540c4b8d
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-09 18:48:44 +0530

    form values read from delete done

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,35 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const Delete = () => {
+
+  const [data,setData] = useState(
+
+    {
+      "userName":""
+    }
+
+  )
+
+  const inputHandler = (event) => {
+
+    setData({...data,[event.target.name]:event.target.value})
+
+  }
+
+  const outputHandler = () => {
+
+    console.log(data)
+
+  }
+
+
   return (
     <div>
-<NavBar/>
-<div className="container">
+      <NavBar />
+      <div className="container">
         <div className="row">
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
             <div className="row g-3">
 
-                <h2>Delete</h2>
+              <h2>Delete</h2>
 
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                 <label htmlFor="" className="form-label">User Name</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name='userName' value={data.userName} onChange={inputHandler} />
 
-                </div>
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                <button className="btn btn-danger">Delete</button>
+                <button className="btn btn-danger" onClick={outputHandler}>Delete</button>
 
-                </div>
+              </div>
             </div>
 
 
-            </div>
+          </div>
         </div>
-    </div>
+      </div>
 
 
     </div>
